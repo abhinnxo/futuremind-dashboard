481aaed3f54ac5984aee109117c16d08403c75d7
Abhinn Krishn <abhinnk21@gmail.com>
2024-04-16 22:28:10 +0530

    FIX: Button width

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,16 +1,11 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 function Button({ image, text }) {
   return (
-    <div className="py-1 px-6 rounded bg-tm-blue hover:bg-tm-blue1 flex justify-center items-center gap-2 cursor-pointer">
-      <Image
-        src={image}
-        alt={text}
-        width="30px"
-        className="text-white font-xl text-white"
-      />
-      {text}
+    <div className="w-fit py-1 px-6 rounded bg-tm-blue hover:bg-tm-blue1 flex justify-center items-center gap-2 cursor-pointer">
+      <Image src={image} alt={text} width="30px" className="font-xl" />
+      <span className="text-white">{text}</span>
     </div>
   );
 }
