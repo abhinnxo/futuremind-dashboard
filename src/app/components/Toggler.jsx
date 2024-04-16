1090300848c14305dd42432da0e9da2d74500a2c
Abhinn Krishn <abhinnk21@gmail.com>
2024-04-17 00:18:59 +0530

    ADD: Toggler Component

---
+++ src/app/components/Toggler.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+
+function Toggler({ text }) {
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <span className="flex items-center gap-1">
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${enabled ? "bg-tm-blue" : "bg-tm-grey"}
+          relative inline-flex h-5 w-8 items-center rounded-full`}
+      >
+        <span className="sr-only">Use setting</span>
+        <span
+          className={`${
+            enabled ? "translate-x-4" : "translate-x-1/4"
+          } inline-block h-3 w-3 transform rounded-full bg-white transition`}
+        />
+      </Switch>
+      <span className="font-medium">{text}</span>
+    </span>
+  );
+}
+
+export default Toggler;
