beb59864f97f41d12e6b34997b2f3ad85ee12889
Abhinn Krishn <abhinnk21@gmail.com>
2024-04-17 13:05:17 +0530

    FIX: color class in Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
 
 function Button({ image, text }) {
   return (
-    <div className="w-fit py-1 px-6 rounded bg-tm-blue hover:bg-tm-blue1 flex justify-center items-center gap-2 cursor-pointer">
+    <div className="w-fit py-1 px-6 rounded bg-primary-500 hover:bg-primary-600 flex justify-center items-center gap-2 cursor-pointer">
       <Image src={image} alt={text} width="30px" className="font-xl" />
       <span className="text-white">{text}</span>
     </div>
