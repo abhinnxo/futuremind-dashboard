d9db37f712e35948e91179e333971844127fc20d
Rishabh Mehta <rishabh.mh1@gmail.com>
2024-04-20 09:42:37 +0530

    husky hook test

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import Image from 'next/image';
 import Link from 'next/link';
