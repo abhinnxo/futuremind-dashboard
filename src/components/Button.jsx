d774fe1d56b35a06b5f8083b844d66e410261473
Abhinn Krishn <abhinnk21@gmail.com>
2024-04-22 00:51:48 +0530

    UPDATE: icon in btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import React from 'react';
 
 function Button({ image, text }) {
   return (
-    <div className="w-fit py-1 px-6 rounded bg-primary-500 hover:bg-primary-600 flex justify-center items-center gap-2 cursor-pointer">
-      <Image src={image} alt={text} width="30px" className="font-xl" />
+    <div className="w-fit py-1 px-6 rounded bg-btn-primary hover:bg-btn-hover flex justify-center items-center gap-2 cursor-pointer">
+      {image}
       <span className="text-white">{text}</span>
     </div>
   );
