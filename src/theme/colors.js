740e913633ce29a5d54a2acdbfc9af736f6b19ca
Abhinn Krishn <abhinnk21@gmail.com>
2024-04-22 01:01:23 +0530

    Added new colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -4,9 +4,17 @@ const PRIMITIVES = {
   },
   secondary: {
     500: '#e6e6e6',
+    300: '#e2e2e2',
   },
   tertiary: {
     500: '#0d0d0db3',
+  },
+};
+
+const BUTTON = {
+  primary: {
+    500: '#3170F9',
+    300: '#0750F0',
   },
 };
 
@@ -15,6 +23,9 @@ const componentColors = {
   'nav-hover': PRIMITIVES.secondary[500],
   'nav-text': PRIMITIVES.tertiary[500],
   'on-hover': PRIMITIVES.primary[500],
+  'secondary-300': PRIMITIVES.secondary[300],
+  'btn-primary': BUTTON.primary[500],
+  'btn-hover': BUTTON.primary[300],
 };
 
 module.exports = {
