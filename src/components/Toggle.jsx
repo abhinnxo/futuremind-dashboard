f72ca53c00f0522bd3618e96412110badb969bcb
Abhinn Krishn <abhinnk21@gmail.com>
2024-04-22 01:41:56 +0530

    CHANGE: custom color classes, text position to left

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -8,10 +8,11 @@ function Toggle({ text }) {
 
   return (
     <span className="flex items-center gap-1">
+      <span className="font-medium">{text}</span>
       <Switch
         checked={enabled}
         onChange={setEnabled}
-        className={`${enabled ? 'nav-active' : 'nav-active'}
+        className={`${enabled ? 'bg-btn-primary' : 'bg-secondary-300'}
           relative inline-flex h-5 w-8 items-center rounded-full`}
       >
         <span className="sr-only">Use setting</span>
@@ -21,7 +22,6 @@ function Toggle({ text }) {
           } inline-block h-3 w-3 transform rounded-full bg-white transition`}
         />
       </Switch>
-      <span className="font-medium">{text}</span>
     </span>
   );
 }
