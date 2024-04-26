fc3c0d0c10e77a4707330d99c96bcac738a26b1d
Abhinn Krishn <abhinnk21@gmail.com>
2024-04-26 23:47:47 +0530

    Added infinite scroll in Table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useEffect, useRef } from 'react';
 import {
   flexRender,
   useReactTable,
@@ -15,6 +15,7 @@ const Table = ({ dataJSON, columnDef, enableSorting }) => {
   const finalColumnDef = useMemo(() => columnDef, [columnDef]);
 
   const [sorting, setSorting] = useState([]);
+  const [visibleRows, setVisibleRows] = useState(15);
 
   const tableInstance = useReactTable({
     columns: finalColumnDef,
@@ -27,10 +28,41 @@ const Table = ({ dataJSON, columnDef, enableSorting }) => {
     onSortingChange: setSorting,
   });
 
+  const tableRef = useRef(null);
+
+  // Function to handle scroll event
+  const handleScroll = () => {
+    const table = tableRef.current;
+    if (table) {
+      const bottom =
+        Math.ceil(table.scrollTop + table.clientHeight) >= table.scrollHeight;
+      if (bottom) {
+        setVisibleRows((prevVisibleRows) => prevVisibleRows + 10); // Increase the number of visible rows by 10
+      }
+    }
+  };
+
+  // Effect to attach scroll event listener to the table
+  useEffect(() => {
+    const table = tableRef.current;
+    if (table) {
+      table.addEventListener('scroll', handleScroll);
+    }
+    return () => {
+      if (table) {
+        table.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+
   return (
-    <div className="bg-white border p-2 rounded-md">
-      <div className="overflow-x-scroll">
-        <table className="w-full">
+    <div
+      className="bg-white border p-2 rounded-md"
+      style={{ height: '500px', overflowY: 'auto' }}
+      ref={tableRef}
+    >
+      <div>
+        <table>
           <thead>
             {tableInstance.getHeaderGroups().map((headerEl) => {
               return (
@@ -70,25 +102,28 @@ const Table = ({ dataJSON, columnDef, enableSorting }) => {
             })}
           </thead>
           <tbody>
-            {tableInstance.getRowModel().rows.map((rowEl) => {
-              return (
-                <tr
-                  key={rowEl.id}
-                  className="border-b-2 py-4 hover:bg-btn-selected text-nowrap"
-                >
-                  {rowEl.getVisibleCells().map((cellEl) => {
-                    return (
-                      <td key={cellEl.id} className="py-2">
-                        {flexRender(
-                          cellEl.column.columnDef.cell,
-                          cellEl.getContext(),
-                        )}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
+            {tableInstance
+              .getRowModel()
+              .rows.slice(0, visibleRows)
+              .map((rowEl) => {
+                return (
+                  <tr
+                    key={rowEl.id}
+                    className="border-b-2 py-4 hover:bg-btn-selected text-nowrap"
+                  >
+                    {rowEl.getVisibleCells().map((cellEl) => {
+                      return (
+                        <td key={cellEl.id} className="py-2">
+                          {flexRender(
+                            cellEl.column.columnDef.cell,
+                            cellEl.getContext(),
+                          )}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
