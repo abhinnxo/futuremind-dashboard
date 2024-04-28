a29fc9160dde81aaa6917d6ab9e158cc8fca6c56
Abhinn Krishn <abhinnk21@gmail.com>
2024-04-28 23:46:34 +0530

    If no text is passed then dont render badge

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 
 function Badge({ type, text }) {
-  if (type === 'primary')
-    return (
-      <span className="bg-blue-500 text-white px-2 py-1 rounded-xl bg-opacity-55">
-        {text}
-      </span>
-    );
-  if (type === 'danger')
-    return (
-      <span className="bg-red-400 text-white px-2 py-1 rounded-xl bg-opacity-75">
-        {text}
-      </span>
-    );
-  if (type === 'warning')
-    return (
-      <span className="bg-yellow-400 text-white px-2 py-1 rounded-xl bg-opacity-75">
-        {text}
-      </span>
-    );
-  if (type === 'success')
-    return (
-      <span className="bg-green-400 text-white px-2 py-1 rounded-xl bg-opacity-75">
-        {text}
-      </span>
-    );
+  if (text !== '') {
+    if (type === 'primary')
+      return (
+        <span className="bg-blue-500 text-white px-2 py-1 rounded-xl bg-opacity-55">
+          {text}
+        </span>
+      );
+    if (type === 'danger')
+      return (
+        <span className="bg-red-400 text-white px-2 py-1 rounded-xl bg-opacity-75">
+          {text}
+        </span>
+      );
+    if (type === 'warning')
+      return (
+        <span className="bg-yellow-400 text-white px-2 py-1 rounded-xl bg-opacity-75">
+          {text}
+        </span>
+      );
+    if (type === 'success')
+      return (
+        <span className="bg-green-400 text-white px-2 py-1 rounded-xl bg-opacity-75">
+          {text}
+        </span>
+      );
+  }
 }
 
 export default Badge;
