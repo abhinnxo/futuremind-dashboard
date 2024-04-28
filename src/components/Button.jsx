0e0df5ec270498d52d06d5fb54bb08c5d2e55853
Abhinn Krishn <abhinnk21@gmail.com>
2024-04-28 13:49:53 +0530

    Added onClick prop to btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function Button({ image, text }) {
+function Button({ image, text, onClick }) {
   return (
-    <div className="w-fit py-1 px-6 rounded bg-btn-primary hover:bg-btn-hover flex justify-center items-center gap-2 cursor-pointer">
+    <button
+      className="w-fit py-1 px-6 rounded bg-btn-primary hover:bg-btn-hover flex justify-center items-center gap-2 cursor-pointer"
+      onClick={onClick}
+    >
       {image}
       <span className="text-white">{text}</span>
-    </div>
+    </button>
   );
 }
 
