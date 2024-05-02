56d05a8769b71e32707b8ea935a2deb7dbbb8912
Abhinn Krishn <abhinnk21@gmail.com>
2024-05-02 19:53:21 +0530

    Updated Badge component with new colors

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
 
 function Badge({ type, text }) {
-  if (text !== '') {
-    if (type === 'primary')
-      return (
-        <span className="bg-blue-500 text-white px-2 py-1 rounded-xl bg-opacity-55">
-          {text}
-        </span>
-      );
-    if (type === 'danger')
-      return (
-        <span className="bg-red-400 text-white px-2 py-1 rounded-xl bg-opacity-75">
-          {text}
-        </span>
-      );
-    if (type === 'warning')
-      return (
-        <span className="bg-yellow-400 text-white px-2 py-1 rounded-xl bg-opacity-75">
-          {text}
-        </span>
-      );
-    if (type === 'success')
-      return (
-        <span className="bg-green-400 text-white px-2 py-1 rounded-xl bg-opacity-75">
-          {text}
-        </span>
-      );
-  }
+  if (type === 'GOLD')
+    return (
+      <span className="bg-yellow-400 text-yellow-800 px-2 py-1 rounded-xl bg-opacity-55">
+        {text}
+      </span>
+    );
+  if (type === 'PREMIUM')
+    return (
+      <span className="bg-blue-400 text-blue-800 px-2 py-1 rounded-xl bg-opacity-75">
+        {text}
+      </span>
+    );
+  if (type === 'BASIC')
+    return (
+      <span className="bg-purple-400 text-purple-800 px-2 py-1 rounded-xl bg-opacity-75">
+        {text}
+      </span>
+    );
+  if (type === 'normal')
+    return (
+      <span className="bg-gray-300 text-gray-800 px-2 py-1 rounded-xl bg-opacity-75">
+        {text}
+      </span>
+    );
 }
 
 export default Badge;
